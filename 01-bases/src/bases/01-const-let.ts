a8206da185ac6f55d34bf286057b40273602fe9d
Carlos <trabajoscharlie@gmail.com>
2026-05-31 23:31:52 -0600

    PRIMER COMMIT DEL PROYECTO VUE

---
+++ 01-bases/src/bases/01-const-let.ts
@@ -0,0 +1,12 @@
+
+
+//var fisrtName = 'Tony';
+//NO DEBEMOS USAR VAR 
+
+
+const fisrtName = 'Tony';
+const lastName = 'Stark';
+
+
+console.log(fisrtName, lastName);
+console.log({fisrtName});
---
+++ 01-bases/src/main.ts
@@ -0,0 +1,23 @@
+import './style.css'
+import typescriptLogo from './assets/typescript.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './bases/01-const-let';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<section id="center">
+  <div class="hero">
+    <img src="${heroImg}" class="base" width="170" height="179">
+    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
+    <img src="${viteLogo}" class="vite" alt="Vite logo" />
+  </div>
+</section>
+
+
+
+
+
+<div class="ticks"></div>
+<section id="spacer"></section>
+`
+console.log("HOLA MUNDO");
